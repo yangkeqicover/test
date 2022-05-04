c4759428240c4bd0c303cab1fe9d7795ec9ec00e
yangyong <yangkeqicover@outlook.com>
2022-05-04 20:00:43 +0800

    第二次提交

---
+++ testing1/test_calc.py
@@ -0,0 +1,14 @@
+from pythoncode1.calcator import Calc
+import pytest
+
+
+@pytest.mark.add
+def test_add1():
+    calc1 = Calc()
+    assert 2 == calc1.add(1, 2)
+
+
+@pytest.mark.add
+def test_add2():
+    calc1 = Calc()
+    assert 2 == calc1.add(1, 2)
